5df607e377749744b6f7de64e88882396b933c55
Ákos Osvald <elettartam@gmail.com>
2026-06-15 18:59:55 +0200

    Report entity is created

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/users.entity';
+import { Report } from './reports/reports.entity';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'better-sqlite3',
     database: 'db.sqlite',
-    entities: [User],
+    entities: [User, Report],
     synchronize: true,
   }), UsersModule, ReportsModule],
   controllers: [AppController],
