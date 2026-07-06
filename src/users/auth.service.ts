16d4cfb44199ef3eaae5db25faab1994228c7b40
Ákos Osvald <elettartam@gmail.com>
2026-07-06 19:50:51 +0200

    hashing of psw and salt generation

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -11,6 +15,10 @@ export class AuthService {
         if (users.length) {
             throw new BadRequestException('Email already in use');
         }
+
+        const salt = randomBytes(8).toString('hex');
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+        const result = salt + '.' + hash.toString('hex');
     }
 
     signin() { }
