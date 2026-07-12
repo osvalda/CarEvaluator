1eba72fe86e4b093183bb5fee846937301c6d437
Ákos Osvald <elettartam@gmail.com>
2026-07-12 12:01:11 +0200

    reorganizing auth tests

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,22 +3,30 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './users.entity';
 
-it('create an instancec of service', async () => {
-    const fakeUserService: Partial<UsersService> = {
-        find: () => Promise.resolve([]),
-        create: (email: string, password: string, name: string) =>
-            Promise.resolve({ id: 1, email, password, name } as User)
-    }
+let service: AuthService;
 
-    const module = await Test.createTestingModule({
-        providers: [
-            AuthService,
-            {
-                provide: UsersService,
-                useValue: fakeUserService
-            }]
-    }).compile();
+describe('AuthService', () => {
+    beforeEach(async () => {
+        const fakeUserService: Partial<UsersService> = {
+            find: () => Promise.resolve([]),
+            create: (email: string, password: string, name: string) =>
+                Promise.resolve({ id: 1, email, password, name } as User)
+        }
 
-    const service = module.get(AuthService);
-    expect(service).toBeDefined();
+        const module = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                {
+                    provide: UsersService,
+                    useValue: fakeUserService
+                }]
+        }).compile();
+
+        service = module.get(AuthService);
+    });
+
+    it('create an instancec of service', async () => {
+        expect(service).toBeDefined();
+    });
+
 });
